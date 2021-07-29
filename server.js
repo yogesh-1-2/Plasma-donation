c2d25c809728f7bc0fd1aff3f356687441d1b5f3
yogesh-1-2 <48681430+yogesh-1-2@users.noreply.github.com>
2021-07-30 04:04:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-const morgan=require('morgan');
+const morgan=require('morgan'); // http request logger middleware
 const cors=require('cors');
 const connectDB=require('./config/db');
 const app=express();
